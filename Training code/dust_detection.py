894a1c1042200929768ce9af6694726e1c6b0dc2
Unknown <cyd.cowley15@ic.ac.uk>
2018-11-06 16:11:39 +0000

    Update Training code

--- Training code/dust_detection.py
+++ Training code/dust_detection.py
@@ -7,7 +7,7 @@ from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import plotly.graph_objs as go
 import imageio as io
 import os
-import cv2
+from sklearn import tree
 
 
 
@@ -41,7 +41,7 @@ def iterate_frames(images):
         print(i)
         images.activeimage=i
         current_frame={"xpositions":[],"ypositions":[],"widths":[], "lengths":[],"pixels":[],"name":"undefined"}
-        [pos1, bgsub1] = images.find_dust(34)
+        [pos1, bgsub1] = images.find_dust(28)
         print(i)
         current_frame["pixels"]=images.collect_dust(pos1)
         print(i)
@@ -144,62 +144,51 @@ class Images:
         dust_this_frame["lengths"]=dust_lengths
         return(dust_this_frame)
 
-    def train(self,distance_cutoff=20,width_cutoff=10):
-        training={"delta_width":[],"delta_position":[],"identifier": []}
+    def connect_frames(self,features,labels):
         """When dust in all frames has been sorted and characterised, this function connects dust particles across frames, forming a trajectory"""
-        
+        clf = tree.DecisionTreeClassifier()
+        clf = clf.fit(features, labels)
+
+
         for i in self.dust_every_frame: #check to ensure characterisation has been performed for every frame
             if i==0:
                 return 0
-        
-        for i in range(len(self.images)-1):
-            current_frame = i
-            next_frame = i + 1
-            print([current_frame,next_frame])
-            for j in range(len(self.dust_every_frame[current_frame]["xpositions"])):
-                for k in range(len(self.dust_every_frame[next_frame]["xpositions"])):
-                    implot0 = plt.imshow(self.images[current_frame]-self.bg)
-                    plt.scatter([self.dust_every_frame[current_frame]["ypositions"][j]], [self.dust_every_frame[current_frame]["xpositions"][j]])
-                    plt.savefig("temp0")
 
-                    plt.clf()
-                    plt.cla()
-                    plt.close()
 
-                    implot1 = plt.imshow(self.images[next_frame]-self.bg)
-                    plt.scatter([self.dust_every_frame[next_frame]["ypositions"][k]],
-                                [self.dust_every_frame[next_frame]["xpositions"][k]])
-                    plt.savefig("temp1")
+        trackx = []
+        tracky = []
+        trackw=[]
 
-                    plt.clf()
-                    plt.cla()
-                    plt.close()
+        for i in range(len(self.dust_every_frame[0]["xpositions"])):
+            trackx.append([self.dust_every_frame[0]["xpositions"][i]])
+            tracky.append([self.dust_every_frame[0]["ypositions"][i]])
+            trackw.append([self.dust_every_frame[0]["widths"][i]])
+        for i in range(len(trackx)):
+            trackframe.append(0)
 
-                    img0 = cv2.imread('temp0.png')
-                    img1 = cv2.imread('temp1.png')
+        for i in range(1,len(self.images)):
+            for j in range(len(self.dust_every_frame[i]["xpositions"])):
+                belongto=False
+                for k in range(len(trackx)):
+                    dw = np.abs(self.dust_every_frame[i]["widths"][j] -
+                                trackw[k][-1])
+                    dp = np.sqrt((self.dust_every_frame[i]["xpositions"][j] -
+                                  trackx[k][-1]) ** 2 +
+                                 (self.dust_every_frame[i]["ypositions"][j] -
+                                  tracky[k][-1]) ** 2)
+                    print(clf.predict([[dw,dp]]))
+                    if clf.predict([[dw, dp]])=="yes":
+                        trackx[k].append(self.dust_every_frame[i]["xpositions"][j])
+                        tracky[k].append(self.dust_every_frame[i]["ypositions"][j])
+                        trackw[k].append(self.dust_every_frame[i]["widths"][j])
+                        belongto=True
 
-                    while (1):
-                        cv2.imshow('img', img0)
-                        cv2.imshow('img1', img1)
-                        key = cv2.waitKey(33)
-                        if key == 27:  # Esc key to stop
-                            break
-                        elif key == 121:  # normally -1 returned,so don't print it
-                            dw = np.abs(self.dust_every_frame[next_frame]["widths"][k]-
-                                        self.dust_every_frame[current_frame]["widths"][j])
-                            dp = np.sqrt((self.dust_every_frame[next_frame]["xpositions"][k]-self.dust_every_frame[current_frame]["xpositions"][j])**2+
-                                        (self.dust_every_frame[next_frame]["ypositions"][k] -self.dust_every_frame[current_frame]["ypositions"][j]) ** 2)
-                            training["delta_width"].append(dw)
-                            training["delta_position"].append(dp)
-                            training["identifier"].append("yes")
-                            break
-                        elif key == 110:
-                            dw = np.abs(self.dust_every_frame[next_frame]["widths"][k]-
-                                        self.dust_every_frame[current_frame]["widths"][j])
-                            dp = np.sqrt((self.dust_every_frame[next_frame]["xpositions"][k]-self.dust_every_frame[current_frame]["xpositions"][j])**2+
-                                        (self.dust_every_frame[next_frame]["ypositions"][k] -self.dust_every_frame[current_frame]["ypositions"][j]) ** 2)
-                            training["delta_width"].append(dw)
-                            training["delta_position"].append(dp)
-                            training["identifier"].append("no")
-                            break
-        return(training)
+                if belongto==False:
+                    trackx.append([self.dust_every_frame[i]["xpositions"][j]])
+                    tracky.append([self.dust_every_frame[i]["ypositions"][j]])
+                    trackw.append([self.dust_every_frame[i]["widths"][j]])
+
+
+
+        return(trackx,tracky)
+
--- Training code/machine_learning.py
+++ Training code/machine_learning.py
@@ -1,18 +1,40 @@
 from sklearn import tree
-import imageprocessing as dd
+import imageprocessing as ip
+import dust_detection as dd
 import matplotlib.pyplot as plt
+import csv
+import numpy as np
 
-im = dd.import_images("KL11-P1DB-92708-3")
 
-set_1 = dd.Images(im)
+im = ip.import_images("KL11-P1DB-92708-3")
+
+set_1 = ip.Images(im)
 set_1.find_bg()
-dd.iterate_frames(set_1)
-set_1.train()
+ip.iterate_frames(set_1)
+training_data=set_1.train()
 
+features=[]
+labels = []
 
-features = [[2200,4,1000], [1500,1,1000], [1800,1,1000], [900,2,0.4], [1000,2,0.4]]
-labels = ['SUV', 'SUV', 'SUV', 'hatchback', 'hatchback']
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(features, labels)
+for i in range(len(training_data["delta_width"])):
+    features.append([])
+for i in range(len(training_data["delta_width"])):
+    features[i].append(training_data["delta_width"][i])
+    features[i].append(training_data["delta_position"][i])
+    labels.append(training_data["identifier"][i])
 
-print(clf.predict([[1350, 1,100]]))
+im2 = ip.import_images("test1")
+set_2 = dd.Images(im2)
+set_2.find_bg()
+dd.iterate_frames(set_2)
+tx,ty=set_2.connect_frames(features,labels)
+
+for i in range(len(tx)):
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+    implot1 = plt.imshow(set_2.images[0] - set_2.bg)
+    plt.scatter(ty[i],tx[i])
+    plt.savefig("tracks/temp"+str(i))
+
