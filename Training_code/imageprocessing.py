e9e8623a0df85d89022e90e953701e00e3b0e0dd
Patrick Fuller <pjf15@ic.ac.uk>
2018-11-12 23:33:24 +0000

    Finalised image processing file

--- Training_code/imageprocessing.py
+++ Training_code/imageprocessing.py
@@ -7,66 +7,68 @@ import imageio as io
 import os
 import cv2
 
-
-
-def rgb2gray(rgb):
-
-    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
-    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
-
-    return gray
-
-
-def import_images(folder):
-    images = []
-    a=os.listdir(folder)  # listdir returns a list of the entries in the folder
-    for image in a[1:]:    
-        img = m.imread(os.path.join(folder,image))  # imread reads an image from a file into an array
-        print(type(img[0][0]))
-        if 'numpy' in str(type(img[0][0])): #checks whether image is rgba or gray
-            img = rgb2gray(img)
-        images.append(img)
-    return(images)
-
-def make_gif(image_list):
-    images = []
-    for filename in image_list:
-        images.append(io.imread("plots/"+filename))
-    io.mimsave('surface1.gif', images, duration=0.5)
-
-def iterate_frames(images):
-    for i in range(len(images.images)):
-        print(i)
-        images.activeimage=i
-        current_frame={"xpositions":[],"ypositions":[],"widths":[], "lengths":[],"pixels":[],"name":"undefined"}
-        [pos1, bgsub1] = images.find_dust(34)
-        current_frame["pixels"]=images.collect_dust(pos1)
-        current_frame = images.characterise_dust(current_frame["pixels"])
-
-
-        images.dust_every_frame[images.activeimage] = current_frame
-
-
-class Images:
-    def __init__(self,images):
-        self.images=images # list of all the pixel data of every image
+class Image_processing:
+    
+    def rgb2gray(rgb):
+        r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+        gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+    
+        return gray
+    
+    def import_images(self,folder):
+        images = []
+        a=os.listdir(folder)  # listdir returns a list of the entries in the folder
+        for image in a[1:]:    
+            img = m.imread(os.path.join(folder,image))  # imread reads an image from a file into an array
+            if 'numpy' in str(type(img[0][0])): #checks whether image is rgba or gray
+                img = Image_processing.rgb2gray(img)
+            images.append(img)
+        return(images)
+        
+        self.images = images # list of all the pixel data of every image
         self.bg = np.zeros_like(self.images[0]) #create a blank slate for background
-        self.activeimage=0 # sets the current image worked on for find_dust, collect_dust, and characterise_dust
-        self.dust_every_frame=len(self.images)*[0]
+        self.dust_every_frame = len(self.images)*[0]
         
     def find_bg(self):
         """Function that returns the average of all the images"""
         try:
             for image in self.images:
                 self.bg += image
-            self.bg = self.bg/len(self.images)
+            self.backgrounds = self.bg/len(self.images)
         except: #pictures could be an empty list, we don't want an error in that case. we just don't do anything to it then
             return 0
+        
+    def variable_bg(self,bgres):
+        """Function that returns a list of the backgrounds for each image - now each image has a 
+        unique background given by the average of a range of images either side of the chosen image"""
+        self.backgrounds=[]
+        for image in range(len(self.images)):
+            self.bg = np.zeros_like(self.images[0])
+            if image < bgres:
+                for i in self.images[0:(2*bgres)+1]:
+                    self.bg += i
+                self.bg = self.bg/((2*bgres)+1)
+                self.backgrounds.append(self.bg)
+                    
+            elif image > len(self.images) - (bgres+1):
+                for i in self.images[-1-(2*bgres):]:
+                    self.bg += i
+                self.bg = self.bg/((2*bgres)+1)
+                self.backgrounds.append(self.bg)
+                    
+            else:
+                for i in self.images[image-bgres:image+bgres+1]:
+                    self.bg += i
+                self.bg = self.bg/((2*bgres)+1)
+                self.backgrounds.append(self.bg)
         
     def find_dust(self,threshold):
         """Function that sets dust images to brightness 1, and stores the positions in dust_positions array"""
         dust_positions=[]
-        bgsubtracted_image = self.images[self.activeimage]-self.bg
+        if type(self.backgrounds) == list:
+            bgsubtracted_image = self.images[self.activeimage]-self.backgrounds[self.activeimage]
+        else:
+            bgsubtracted_image = self.images[self.activeimage]-self.backgrounds
         for i in range(len(bgsubtracted_image)):
             for j in range(len(bgsubtracted_image[0])):
                 if bgsubtracted_image[i][j]>=threshold:
@@ -76,24 +78,24 @@ class Images:
                     bgsubtracted_image[i][j]=0.0
         return([dust_positions,bgsubtracted_image])
         
-    def collect_dust(self,positions):
+    def collect_dust(self,pixels):
         """Function that lumps dust pixels into dust grains, by checking if the bright pixels neighbor other bright pixels"""
         
         dust_grains = []
-        dust_grains.append([positions[0]])
-        positions.pop(0)
+        dust_grains.append([pixels[0]])
+        pixels.pop(0)
         
-        while(len(positions)>=1):
+        while(len(pixels)>=1):
             contained = False
             for i in range(len(dust_grains)):
                 for j in range(len(dust_grains[i])):
-                    if(np.absolute(positions[0][0]-dust_grains[i][j][0])<=1 and np.absolute(positions[0][1]-dust_grains[i][j][1])<=1):
+                    if(np.absolute(pixels[0][0]-dust_grains[i][j][0])<=1 and np.absolute(pixels[0][1]-dust_grains[i][j][1])<=1):
                         contained = i
             if type(contained)==int:
-                dust_grains[contained].append(positions[0])
+                dust_grains[contained].append(pixels[0])
             else:
-                dust_grains.append([positions[0]])
-            positions.pop(0)
+                dust_grains.append([pixels[0]])
+            pixels.pop(0)
         keep_dustgrains=[]
         
         for grain in (dust_grains):
@@ -101,8 +103,6 @@ class Images:
                 keep_dustgrains.append(grain)
         return(keep_dustgrains)
         
-        
-
     def characterise_dust(self,pixels):
         """Funciton that takes pixel locations of each dust grain and outpus entire dust grain position and dimensions"""
         dust_this_frame={"xpositions":[],"ypositions":[],"widths":[], "lengths":[],"pixels":[],"name":"undefined"}
@@ -133,11 +133,50 @@ class Images:
             dust_xpositions[i] = av_x
             dust_ypositions[i] = av_y
 
-        dust_this_frame["pixels"]
-        dust_this_frame["xpositions"]= dust_xpositions
+        dust_this_frame["pixels"]=pixels
+        dust_this_frame["xpositions"]=dust_xpositions
         dust_this_frame["ypositions"]=dust_ypositions
-        dust_this_frame["widths"]= dust_widths
+        dust_this_frame["widths"]=dust_widths
         dust_this_frame["lengths"]=dust_lengths
         return(dust_this_frame)
+    
+    def iterate_frames(self):
+        for i in range(len(self.images)):
+            self.activeimage=i
+            current_frame={"xpositions":[],"ypositions":[],"widths":[], "lengths":[],"pixels":[],"name":"undefined"}
+            [positions, bgsub_image] = self.find_dust(34)
+            current_frame["pixels"]=self.collect_dust(positions)
+            current_frame = self.characterise_dust(current_frame["pixels"])
+            self.dust_every_frame[self.activeimage] = current_frame
+    
+    def make_gif(self,image_list):
+        images = []
+        for filename in image_list:
+            images.append(io.imread("plots/"+filename))
+        io.mimsave('surface1.gif', images, duration=0.5)
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
    
