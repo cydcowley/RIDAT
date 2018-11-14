65db86a89a5aa71a0ca7e51559722bfb2acccd18
Unknown <cyd.cowley15@ic.ac.uk>
2018-11-14 13:54:15 +0000

    Updated everything

--- Training_code/dust_detection.py
+++ Training_code/dust_detection.py
@@ -8,43 +8,54 @@ import os
 from sklearn import tree
 import cv2
 
-def train(self):
-    
-    training={"delta_width":[],"delta_position":[],"identifier": []}
-    
+
+def train(dust_dictionary,images):
     """When dust in all frames has been sorted and characterised, this function connects dust particles across frames, forming a trajectory"""
-    
-    for i in self.dust_every_frame: #check to ensure characterisation has been performed for every frame
-        if i==0:
-            return 0
-    
-    for i in range(len(self.images)-1):
+
+    training = {"delta_width": [], "delta_position": [], "delta_theta": [],
+                "identifier": []}  # define an empty dictionary of machine learning parameters
+
+    for i in range(len(dust_dictionary) - 1):
+
         current_frame = i
         next_frame = i + 1
-        for j in range(len(self.dust_every_frame[current_frame]["xpositions"])):
-            for k in range(len(self.dust_every_frame[next_frame]["xpositions"])):
-                
-                dw = np.abs(self.dust_every_frame[next_frame]["widths"][k]-
-                            self.dust_every_frame[current_frame]["widths"][j])
-                dp = np.sqrt((self.dust_every_frame[next_frame]["xpositions"][k]-self.dust_every_frame[current_frame]["xpositions"][j])**2+
-                             (self.dust_every_frame[next_frame]["ypositions"][k] -self.dust_every_frame[current_frame]["ypositions"][j]) ** 2)
-                
+
+        for j in range(len(
+                dust_dictionary[current_frame]["x0s"])):  # cycle through every dust grain combination across two frames
+            for k in range(len(dust_dictionary[next_frame]["x0s"])):
+
+                avx0 = (dust_dictionary[current_frame]["x0s"][j] + dust_dictionary[current_frame]["x1s"][j]) / 2
+                avx1 = (dust_dictionary[next_frame]["x0s"][k] + dust_dictionary[next_frame]["x1s"][k]) / 2
+                avy0 = (dust_dictionary[current_frame]["y0s"][j] + dust_dictionary[current_frame]["y1s"][j]) / 2
+                avy1 = (dust_dictionary[next_frame]["y0s"][k] + dust_dictionary[next_frame]["y1s"][k]) / 2
+
+                theta0 = np.arctan2(
+                    (dust_dictionary[current_frame]["y0s"][j] - dust_dictionary[current_frame]["y1s"][j]),
+                    (dust_dictionary[current_frame]["x0s"][j] - dust_dictionary[current_frame]["x1s"][j]))
+                theta1 = np.arctan2((dust_dictionary[next_frame]["y0s"][k] - dust_dictionary[next_frame]["y1s"][k]),
+                                    (dust_dictionary[next_frame]["x0s"][k] - dust_dictionary[next_frame]["x1s"][k]))
+
+                dw = np.abs(dust_dictionary[next_frame]["widths"][k] -
+                            dust_dictionary[current_frame]["widths"][j])
+                dp = np.sqrt((avx1 - avx0) ** 2 + (avy1 - avy0) ** 2)
+
+                dtheta = np.abs(theta1 - theta0)
+
                 training["delta_width"].append(dw)
                 training["delta_position"].append(dp)
-                
-                implot0 = plt.imshow(self.images[current_frame]-self.bg)
-                plt.scatter([self.dust_every_frame[current_frame]["ypositions"][j]], [self.dust_every_frame[current_frame]["xpositions"][j]])
-                plt.savefig("temp0")
+                training["delta_theta"].append(dtheta)
 
+                implot0 = plt.imshow(images[current_frame])
+                plt.scatter([avy0], [avx0])
+                plt.savefig("temp0")
                 plt.clf()
                 plt.cla()
                 plt.close()
 
-                implot1 = plt.imshow(self.images[next_frame]-self.bg)
-                plt.scatter([self.dust_every_frame[next_frame]["ypositions"][k]],
-                            [self.dust_every_frame[next_frame]["xpositions"][k]])
-                plt.savefig("temp1")
+                implot1 = plt.imshow(images[next_frame])
+                plt.scatter([avy1], [avx1])
 
+                plt.savefig("temp1")
                 plt.clf()
                 plt.cla()
                 plt.close()
@@ -52,7 +63,7 @@ def train(self):
                 img0 = cv2.imread('temp0.png')
                 img1 = cv2.imread('temp1.png')
 
-                while (1):
+                while True:  # displays current and next frame until a key is pressed
                     cv2.imshow('img', img0)
                     cv2.imshow('img1', img1)
                     key = cv2.waitKey(33)
@@ -64,56 +75,66 @@ def train(self):
                     elif key == 110:
                         training["identifier"].append("no")
                         break
-    return(training)
+    return training
 
 
 
-def connect_frames(self,features,labels):
+def track(dust_dictionary,images,features,labels):
     """When dust in all frames has been sorted and characterised, this function connects dust particles across frames, forming a trajectory"""
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(features, labels)
 
-
-    for i in self.dust_every_frame: #check to ensure characterisation has been performed for every frame
-        if i==0:
-            return 0
-
-
     trackx = []
     tracky = []
     trackw=[]
-    trackframe=[]
+    track_theta=[]
+    track_lastframe=[] #defines the last frame where a dust grain in a given track was recorded
 
-    for i in range(len(self.dust_every_frame[0]["xpositions"])):
-        trackx.append([self.dust_every_frame[0]["xpositions"][i]])
-        tracky.append([self.dust_every_frame[0]["ypositions"][i]])
-        trackw.append([self.dust_every_frame[0]["widths"][i]])
+    for i in range(len(dust_dictionary[0]["x0s"])):
+        avx = (dust_dictionary[0]["x0s"][i] + dust_dictionary[0]["x1s"][i]) / 2
+        avy = (dust_dictionary[0]["y0s"][i] + dust_dictionary[0]["y1s"][i]) / 2
+        theta = np.arctan2((dust_dictionary[0]["y0s"][i] - dust_dictionary[0]["y1s"][i]),
+            (dust_dictionary[0]["x0s"][i] - dust_dictionary[0]["x1s"][i]))
+
+        trackx.append([avx])
+        tracky.append([avy])
+        track_theta.append([theta])
+        trackw.append([dust_dictionary[0]["widths"][i]])
+
     for i in range(len(trackx)):
-        trackframe.append(0)
+        track_lastframe.append(0)
 
-    for i in range(1,len(self.images)):
-        for j in range(len(self.dust_every_frame[i]["xpositions"])):
-            belongto=False
+    for i in range(1,len(images)):
+        for j in range(len(dust_dictionary[i]["x0s"])):
+            belong_to = False
+
+            avx = (dust_dictionary[i]["x0s"][j] + dust_dictionary[i]["x1s"][j]) / 2
+            avy = (dust_dictionary[i]["y0s"][j] + dust_dictionary[i]["y1s"][j]) / 2
+            theta = np.arctan2((dust_dictionary[i]["y0s"][j] - dust_dictionary[i]["y1s"][j]),
+                               (dust_dictionary[i]["x0s"][j] - dust_dictionary[i]["x1s"][j]))
+
+
             for k in range(len(trackx)):
-                dw = np.abs(self.dust_every_frame[i]["widths"][j] -
+
+                dw = np.abs(dust_dictionary[i]["widths"][j] -
                             trackw[k][-1])
-                dp = np.sqrt((self.dust_every_frame[i]["xpositions"][j] -
-                              trackx[k][-1]) ** 2 +
-                             (self.dust_every_frame[i]["ypositions"][j] -
-                              tracky[k][-1]) ** 2)
-                print(clf.predict([[dw,dp]]))
-                if clf.predict([[dw, dp]])=="yes":
-                    trackx[k].append(self.dust_every_frame[i]["xpositions"][j])
-                    tracky[k].append(self.dust_every_frame[i]["ypositions"][j])
-                    trackw[k].append(self.dust_every_frame[i]["widths"][j])
-                    belongto=True
+                dp = np.sqrt((avx -trackx[k][-1]) ** 2 +(avy -tracky[k][-1]) ** 2)
+                dtheta= np.abs(theta-track_theta[k][-1])
 
-            if belongto==False:
-                trackx.append([self.dust_every_frame[i]["xpositions"][j]])
-                tracky.append([self.dust_every_frame[i]["ypositions"][j]])
-                trackw.append([self.dust_every_frame[i]["widths"][j]])
+                if clf.predict([[dw, dp,dtheta]]) == "yes" and track_lastframe[k] >= i-5:
+                    trackx[k].append(avx)
+                    tracky[k].append(avy)
+                    track_theta[k].append(theta)
+                    trackw[k].append(dust_dictionary[i]["widths"][j])
+                    belong_to = True
+                    track_lastframe[k]=i
 
+            if not belong_to:
+                trackx.append([avx])
+                tracky.append([avy])
+                track_theta.append([theta])
+                trackw.append([dust_dictionary[i]["widths"][j]])
+                track_lastframe.append(i)
 
-
-    return(trackx,tracky)
+    return (trackx,tracky)
 
--- Training_code/run_file.py
+++ Training_code/run_file.py
@@ -4,14 +4,34 @@ import dust_detection as dd
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
+import json
+import os
 
 
-im = ip.import_images("KL11-P1DB-92708-3")
+set_1=ip.import_images("KL11-P1DB-92832-1")
+dict,bgsub=ip.iterate_frames(set_1,30)
+# training_data=dd.train(dict, bgsub)
+#
+# json = json.dumps(training_data)
+# f = open("training_data/KL11-P1DB-92832-1.json", "w")
+# f.write(json)
+# f.close()
 
-set_1 = ip.Images(im)
-set_1.find_bg()
-ip.iterate_frames(set_1)
-training_data=set_1.train()
+
+
+training_data={"delta_width": [], "delta_position": [], "delta_theta": [],
+                "identifier": []}
+
+a=os.listdir("training_data")  # listdir returns a list of the entries in the folder
+for image in a:
+    print(image)
+    f1 = open("training_data/"+str(image))
+    data=json.load(f1)
+    training_data["delta_width"].extend(data["delta_width"])
+    training_data["delta_position"].extend(data["delta_position"])
+    training_data["delta_theta"].extend(data["delta_theta"])
+    training_data["identifier"].extend(data["identifier"])
+
 
 features=[]
 labels = []
@@ -21,19 +41,20 @@ for i in range(len(training_data["delta_width"])):
 for i in range(len(training_data["delta_width"])):
     features[i].append(training_data["delta_width"][i])
     features[i].append(training_data["delta_position"][i])
+    features[i].append(training_data["delta_theta"][i])
     labels.append(training_data["identifier"][i])
 
-im2 = ip.import_images("test1")
-set_2 = dd.Images(im2)
-set_2.find_bg()
-dd.iterate_frames(set_2)
-tx,ty=set_2.connect_frames(features,labels)
+print(features[0])
+tx,ty=dd.track(dust_dictionary=dict,images=set_1,features=features,labels=labels)
+
+
+#
 
 for i in range(len(tx)):
     plt.clf()
     plt.cla()
     plt.close()
-    implot1 = plt.imshow(set_2.images[0] - set_2.bg)
-    plt.scatter(ty[i],tx[i])
+    implot1 = plt.imshow(set_1[i])
+    plt.scatter(ty[i],tx[i],c='r')
     plt.savefig("tracks/temp"+str(i))
 
