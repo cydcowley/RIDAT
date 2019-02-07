52cddaa26b854d4d4f6ed33699667ba0438f62e2
Patrick Fuller <pjf15@ic.ac.uk>
2019-02-07 12:33:54 +0000

    Dust Detection with splitting

--- Training_code/dust_detection.py
+++ Training_code/dust_detection.py
@@ -10,27 +10,28 @@ from sklearn.naive_bayes import GaussianNB
 import cv2
 
 
-def find_dp_detheta_avtheta(position_listx,position_listy):
+def find_dp_dtheta_avtheta(position_listx,position_listy):  # uses information on distance travelled within frames as well as between frames
     dp_list=[]
     angle_list=[]
-    dytotoal=0
+    dytotal=0
     dxtotal=0
+    delta_angle_list=[]
     for i in range(0,len(position_listx)-1):
         dy=position_listy[i+1]-position_listy[i]
         dx=position_listx[i+1]-position_listx[i]
-        dytotoal+=dy
+        dytotal+=dy
         dxtotal+=dx
         dp_list.append(np.sqrt(dy**2+dx**2))
         angle = np.arctan2(dy,dx)
         angle_list.append(angle)
-    delta_angle_list=[]
+    
     for i in range(0,len(angle_list)-1):
-        d_angle = np.sqrt((angle_list[i+1]-angle_list[i])**2)
+        d_angle = np.abs(angle_list[i+1]-angle_list[i])
         if d_angle > np.pi:
-            d_angle = 2*np.pi-d_angle
+            d_angle = 2*np.pi-d_angle  
         delta_angle_list.append(d_angle)
 
-    return dp_list,np.mean(delta_angle_list),np.arctan2(dytotoal,dxtotal)
+    return dp_list,np.mean(delta_angle_list),np.arctan2(dytotal,dxtotal)
 
 
 def sort_points(unordered_pointsx, unordered_pointsy):
@@ -50,17 +51,16 @@ def sort_points(unordered_pointsx, unordered_pointsy):
     unordered_pointsx.insert(2, x1)
     unordered_pointsy.insert(2, y1)
 
-    for i in range(3,len(unordered_pointsx)-2,2):
-
+    for i in range(3,len(unordered_pointsx)-2,2): 
         d1=np.sqrt((unordered_pointsx[i+1]-unordered_pointsx[i])**2+(unordered_pointsy[i+1]-unordered_pointsy[i])**2)
         d2=np.sqrt((unordered_pointsx[i+2]-unordered_pointsx[i])**2+(unordered_pointsy[i+2]-unordered_pointsy[i])**2)
-        if d2<d1:
+        if d2<d1:  # if d2 is smaller than d1 swap the coordinates
             x0 = unordered_pointsx.pop(i+1)
             y0 = unordered_pointsy.pop(i+1)
             unordered_pointsx.insert(i+2, x0)
             unordered_pointsy.insert(i+2, y0)
     return unordered_pointsx,unordered_pointsy
-
+  
 
 def train(dust_dictionary,images,streak):
     """When dust in all frames has been sorted and characterised, this function connects dust particles across frames, forming a trajectory"""
@@ -84,7 +84,7 @@ def train(dust_dictionary,images,streak):
     def nearest_point_to_mouse(mousex, mousey, dustx, dusty):
         dist_min = 1000000
         for i in range(len(dustx)):
-            dist = np.sqrt((mousex - dusty[i]) ** 2 + (mousey - dustx[i]) ** 2)
+            dist = np.sqrt((mousex - dustx[i]) ** 2 + (mousey - dusty[i]) ** 2)  
             if dist < dist_min:
                 dist_min = dist
                 index_min = i
@@ -112,7 +112,7 @@ def train(dust_dictionary,images,streak):
             y2=dust_dictionary[frame_2]["y0s"][index2]
 
     training = {"sigma_delta_position": [], "mean_delta_position": [], "mean_delta_theta": [], "mean_delta_width": [],
-                "mean_delta_brightness": [], "mean_theta":[], "delta_brightness":[], "identifier": []}  # define an empty dictionary of machine learning parameters
+                "mean_delta_brightness": [], "mean_theta":[], "identifier": []}  # define an empty dictionary of machine learning parameters
     stopping=False
     for frame in range(len(dust_dictionary) - 2):
         
@@ -124,11 +124,11 @@ def train(dust_dictionary,images,streak):
         global x0,y0,x1,y1,x2,y2,index0,index1,index2
         x0,y0,x1,y1,x2,y2,index0,index1,index2=0,0,0,0,0,0,0,0,0
 
-        while nextframe== False:
+        while nextframe == False:
 
             create_tempimage(frame_0,0,x0,y0)
             create_tempimage(frame_1, 1, x1, y1)
-            create_tempimage(frame_2, 2, x2, y2)
+            create_tempimage(frame_2, 2, x2, y2)  
 
                 
             img0 = cv2.imread('temp0.png')
@@ -144,17 +144,17 @@ def train(dust_dictionary,images,streak):
                 cv2.setMouseCallback("img2", onMouse2)
 
                 key = cv2.waitKey(33)
-                print(key)
-                if key ==27:
+                #print(key)
+                if key ==27:  # esc key
                     stopping = True
                     nextframe=True
                     break
-                if key ==13:
+                if key ==13:  # enter
                     break
                 elif key == 110:
                     nextframe = True
                     break
-                elif key == 115:
+                elif key == 115:  # S
                     def append_variables():
                         trackx = []
                         tracky = []
@@ -165,29 +165,27 @@ def train(dust_dictionary,images,streak):
                         trackx.append(dust_dictionary[frame_0]["x0s"][index0])
                         tracky.append(dust_dictionary[frame_0]["y0s"][index0])
                         trackw.append(dust_dictionary[frame_0]["widths"][index0])
-                        trackb.append(dust_dictionary[frame_2]["brightness"][index0])
+                        trackb.append(dust_dictionary[frame_0]["brightness"][index0])
                         if streak == True:
                             trackx.append(dust_dictionary[frame_0]["x1s"][index0])
                             tracky.append(dust_dictionary[frame_0]["y1s"][index0])
                             trackw.append(dust_dictionary[frame_0]["widths"][index0])
-                            trackb.append(dust_dictionary[frame_2]["brightness"][index0])
-
+                            trackb.append(dust_dictionary[frame_0]["brightness"][index0])
 
                         # append unordered positions and widths of particle j frame 1 to track lists
-
+    
                         trackx.append(dust_dictionary[frame_1]["x0s"][index1])
                         tracky.append(dust_dictionary[frame_1]["y0s"][index1])
                         trackw.append(dust_dictionary[frame_1]["widths"][index1])
-                        trackb.append(dust_dictionary[frame_2]["brightness"][index1])
+                        trackb.append(dust_dictionary[frame_1]["brightness"][index1])
                         if streak == True:
                             trackx.append(dust_dictionary[frame_1]["x1s"][index1])
                             tracky.append(dust_dictionary[frame_1]["y1s"][index1])
                             trackw.append(dust_dictionary[frame_1]["widths"][index1])
-                            trackb.append(dust_dictionary[frame_2]["brightness"][index1])
-
+                            trackb.append(dust_dictionary[frame_1]["brightness"][index1])
 
                         # append unordered positions and widths of particle j frame 1 to track lists
-
+    
                         trackx.append(dust_dictionary[frame_2]["x0s"][index2])
                         tracky.append(dust_dictionary[frame_2]["y0s"][index2])
                         trackw.append(dust_dictionary[frame_2]["widths"][index2])
@@ -197,13 +195,15 @@ def train(dust_dictionary,images,streak):
                             tracky.append(dust_dictionary[frame_2]["y1s"][index2])
                             trackw.append(dust_dictionary[frame_2]["widths"][index2])
                             trackb.append(dust_dictionary[frame_2]["brightness"][index2])
+                            
                         if streak == True:
-                            trackx, tracky = sort_points(trackx, tracky)
-                        track_dist, mean_delta_theta, mean_theta = find_dp_detheta_avtheta(trackx, tracky)
+                            trackx, tracky = sort_points(trackx, tracky)  
+                        track_dist, mean_delta_theta, mean_theta = find_dp_dtheta_avtheta(trackx, tracky)
                         mean_delta_position = np.mean(track_dist)
                         sigma_delta_position = np.std(track_dist)
                         mean_delta_width = np.std(trackw)
                         mean_delta_brightness = np.std(trackb)
+
 
                         training["sigma_delta_position"].append(sigma_delta_position)
                         training["mean_delta_position"].append(mean_delta_position)
@@ -223,13 +223,15 @@ def train(dust_dictionary,images,streak):
                             append_variables()
                             training["identifier"].append(0)
                     break
-
+                elif key == 110:  # N
+                    nextframe=True
+                    break
         if stopping==True:
             break
     return training
 
 
-def track(dust_dictionary,features,labels,streak,Threshold_probability):
+def track(dust_dictionary,features,labels,streak,Threshold_probability,splitting_prob,split_switch):
     """When dust in all frames has been sorted and characterised, this function connects dust particles across frames, forming a trajectory"""
     clf = GaussianNB()
     clf = clf.fit(features, labels)
@@ -241,16 +243,23 @@ def track(dust_dictionary,features,labels,streak,Threshold_probability):
     track_lastframe=[[]] #defines the last frame where a dust grain in a given track was recorded
     
     for frame in range(len(dust_dictionary) - 2):
-        print(frame)
         frame_0 = frame
         frame_1 = frame + 1
         frame_2 = frame + 2
-
+        
         for i in range(len(dust_dictionary[frame_0]["x0s"])):  # cycle through every dust grain combination across three frames
             prob0=Threshold_probability
+            splitting_prob = Threshold_probability
+            splitting = False
+            probs=[]
+            trackxfinal=[]
+            trackyfinal=[]
+            trackwfinal=[]
+            trackbfinal=[]
+            trackframefinal=[]
             for j in range(len(dust_dictionary[frame_1]["x0s"])):
                 for k in range(len(dust_dictionary[frame_2]["x0s"])):
-
+                
                     trackx = []
                     tracky = []
                     trackw = []
@@ -279,7 +288,7 @@ def track(dust_dictionary,features,labels,streak,Threshold_probability):
                         trackw.append(dust_dictionary[frame_1]["widths"][j])
                         trackb.append(dust_dictionary[frame_1]["brightness"][j])
 
-                    # append unordered positions and widths of particle j frame 1 to track lists
+                    # append unordered positions and widths of particle k frame 2 to track lists
 
                     trackx.append(dust_dictionary[frame_2]["x0s"][k])
                     tracky.append(dust_dictionary[frame_2]["y0s"][k])
@@ -293,55 +302,95 @@ def track(dust_dictionary,features,labels,streak,Threshold_probability):
 
                     if streak == True:
                         trackx, tracky = sort_points(trackx, tracky)
-                    track_dist, mean_delta_theta, mean_theta = find_dp_detheta_avtheta(trackx, tracky)
+                    track_dist, mean_delta_theta, mean_theta = find_dp_dtheta_avtheta(trackx, tracky)  
                     mean_delta_position = np.mean(track_dist)
                     sigma_delta_position = np.std(track_dist)
                     mean_delta_width = np.std(trackw)
                     mean_delta_brightness = np.std(trackb)
-                    prob1 = clf.predict_proba([[sigma_delta_position,mean_delta_position,mean_delta_theta,mean_delta_width,mean_theta,mean_delta_brightness]])[0][1]
-
-                    if prob1 > prob0:
-                        prob0 = prob1
-
-                        trackxfinal=trackx
-                        trackyfinal=tracky
-                        trackwfinal=trackw
-                        trackbfinal=trackb
-                        if streak == True:
-                            trackframefinal=[frame_0,frame_0,frame_1,frame_1,frame_2,frame_2]
-                        else:
-                            trackframefinal = [frame_0, frame_1, frame_2]
-            if prob0 > float(Threshold_probability):
+                    prob1 = clf.predict_proba([[sigma_delta_position,mean_delta_position,mean_delta_theta,mean_delta_width]])[0][1]
+                    probs.append(prob1)
+                    if split_switch == True:
+                        for l in range(len(probs)-1):
+                            if abs(probs[-1]-probs[l])<=0.01 and probs[-1]>=splitting_prob and probs[l]>=splitting_prob:
+                                splitting = True
+                                trackxfinal.append(trackx)
+                                trackyfinal.append(tracky)
+                                trackwfinal.append(trackw)
+                                trackbfinal.append(trackb)
+                                splitting_prob = prob1
+                                if streak == True:
+                                    trackframefinal.append([frame_0,frame_0,frame_1,frame_1,frame_2,frame_2])
+                                else:
+                                    trackframefinal.append([frame_0, frame_1, frame_2])
+                                break
+                    else:    
+                        if prob1 > prob0 and splitting == False:
+                            prob0 = prob1
+                            
+                            trackxfinal=[trackx]
+                            trackyfinal=[tracky]
+                            trackwfinal=[trackw]
+                            trackbfinal=[trackb]
+                            if streak == True:
+                                trackframefinal=[[frame_0,frame_0,frame_1,frame_1,frame_2,frame_2]]
+                            else:
+                                trackframefinal = [[frame_0, frame_1, frame_2]]
+        
+            if prob0 > Threshold_probability:
                 contained=False
                 for y in range(len(trackxtotal)):
                     for z in range(len(trackxtotal[y])):
-                        if (trackxtotal[y][z]==trackxfinal[0] and trackytotal[y][z] == trackyfinal[0]):
-                            contained = [y,z]
-                if contained == False:
-                    trackxtotal.append(trackxfinal)
-                    trackytotal.append(trackyfinal)
-                    trackwtotal.append(trackwfinal)
-                    trackbtotal.append(trackbfinal)
-                    track_lastframe.append(trackframefinal)
-                else:
-                    #if current total track has overextended slightly, cut it short
-
+                        if (trackxtotal[y][z]==trackxfinal[0] and trackytotal[y][z] == trackyfinal[0]) and track_lastframe[y][z] == trackframefinal[0]:  
+                            contained = [y,z]                                                           
+                if contained == False:  
+                    if splitting == True:
+                        for t in range(len(trackxfinal)):
+                            trackxtotal.append(trackxfinal[t])
+                            trackytotal.append(trackyfinal[t])
+                            trackwtotal.append(trackwfinal[t])
+                            track_lastframe.append(trackframefinal[t])
+                            trackbtotal.append(trackbfinal[t])
+                        
+                    else:
+                        trackxtotal.append(trackxfinal[0])
+                        trackytotal.append(trackyfinal[0])
+                        trackwtotal.append(trackwfinal[0])
+                        trackbtotal.append(trackbfinal[0])
+                        track_lastframe.append(trackframefinal[0])
+                        
+                    
+                else: #if current total track has overextended slightly, cut it short
                     trackxtotal[contained[0]] = trackxtotal[contained[0]][0:contained[1]]
                     trackytotal[contained[0]] = trackytotal[contained[0]][0:contained[1]]
                     trackwtotal[contained[0]] = trackwtotal[contained[0]][0:contained[1]]
                     trackbtotal[contained[0]] = trackbtotal[contained[0]][0:contained[1]]
                     track_lastframe[contained[0]] = track_lastframe[contained[0]][0:contained[1]]
-
-                    trackxtotal[contained[0]].extend(trackxfinal)
-                    trackytotal[contained[0]].extend(trackyfinal)
-                    trackwtotal[contained[0]].extend(trackwfinal)
-                    trackbtotal[contained[0]].extend(trackbfinal)
-                    track_lastframe[contained[0]].extend(trackframefinal)
-
-    trackxtotal.pop(0)
+                    
+                    if splitting == True:
+                        for t in range(1,len(trackxfinal)):
+                            trackxtotal.insert(contained[0]+t,list(trackxtotal[contained[0]]))
+                            trackytotal.insert(contained[0]+t,list(trackytotal[contained[0]]))
+                            trackwtotal.insert(contained[0]+t,list(trackwtotal[contained[0]]))
+                            trackbtotal.insert(contained[0]+t,list(trackbtotal[contained[0]]))
+                            track_lastframe.insert(contained[0]+t,list(track_lastframe[contained[0]]))
+                        for t in range(len(trackxfinal)):
+                            trackxtotal[contained[0]+t].extend(trackxfinal[t])
+                            trackytotal[contained[0]+t].extend(trackyfinal[t])
+                            trackwtotal[contained[0]+t].extend(trackwfinal[t])
+                            trackbtotal[contained[0]+t].extend(trackbfinal[t])
+                            track_lastframe[contained[0]+t].extend(trackframefinal[t])
+                    
+                    else:
+                        trackxtotal[contained[0]].extend(trackxfinal[0])
+                        trackytotal[contained[0]].extend(trackyfinal[0])
+                        trackwtotal[contained[0]].extend(trackwfinal[0])
+                        trackbtotal[contained[0]].extend(trackbfinal[0])
+                        track_lastframe[contained[0]].extend(trackframefinal[0])
+                        
+    trackxtotal.pop(0)   # to get rid of the empty list at the start of trackxtotal
     trackytotal.pop(0)
     trackwtotal.pop(0)
     trackbtotal.pop(0)
     track_lastframe.pop(0)
-    return (trackxtotal,trackytotal,trackbtotal,track_lastframe)
+    return (trackxtotal,trackytotal,trackbtotal,track_lastframe,probs)
 
